2f015b2d0ece540f1279ce1c913e843501c4d89f
celinechole <github@celinechole.com>
2019-05-07 20:08:28 -0400

    add Materialize

--- bon-app/src/App.js
+++ bon-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import "../node_modules/materialize-css/dist/css/materialize.css";
+import "../node_modules/materialize-css/dist/js/materialize.js";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
+    <div className="container">
+      <div className="row">
         Food Planner
       </div>
     </div>
