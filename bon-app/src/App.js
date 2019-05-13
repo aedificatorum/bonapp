37472627467752892ccc5293d849f0ee2dd896f5
celinechole <github@celinechole.com>
2019-05-13 10:20:10 -0400

    fix footer

--- bon-app/src/App.js
+++ bon-app/src/App.js
@@ -4,15 +4,19 @@ import "../node_modules/materialize-css/dist/css/materialize.css";
 import "../node_modules/materialize-css/dist/js/materialize.js";
 import Planner from "./Planner";
 import Navbar from "./Navbar";
-import Footer from "./Footer"
+import Footer from "./Footer";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <HashRouter>
       <React.Fragment>
-        <Navbar />
-        <Planner />
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <Planner />
+        </main>
         <Footer />
       </React.Fragment>
     </HashRouter>
