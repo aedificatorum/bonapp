dd2230064e9cd37ab74c4324d7e429ae16b1176d
celinechole <github@celinechole.com>
2019-05-13 10:18:28 -0400

    implement footer

--- bon-app/src/App.js
+++ bon-app/src/App.js
@@ -4,6 +4,7 @@ import "../node_modules/materialize-css/dist/css/materialize.css";
 import "../node_modules/materialize-css/dist/js/materialize.js";
 import Planner from "./Planner";
 import Navbar from "./Navbar";
+import Footer from "./Footer"
 import { HashRouter, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <React.Fragment>
         <Navbar />
         <Planner />
+        <Footer />
       </React.Fragment>
     </HashRouter>
   );
