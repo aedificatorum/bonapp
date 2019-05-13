6e4c9c9ebd6f7ef4ece880e261902418c520dfce
celinechole <github@celinechole.com>
2019-05-13 11:25:51 -0400

    test with function component

--- bon-app/src/Planner.js
+++ bon-app/src/Planner.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 const Planner = () => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem('myValueInLocalStorage') || ''
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem('myValueInLocalStorage', value);
+  }, [value]);
+
+  const onChange = event => setValue(event.target.value);
+
   return (
     <div className="container">
       <div class="row">
         <form class="col s12">
           <div class="row">
             <div class="input-field col s12 m6">
-              <textarea id="mondayLunch" class="materialize-textarea" />
+              <textarea id="mondayLunch" class="materialize-textarea" value={value} onChange={onChange}/>
               <label for="mondayLunch">Monday Lunch</label>
             </div>
             <div class="input-field col s12 m6">
