0cba6fdea951a1449c5e9789b22670b653c36af8
celinechole <github@celinechole.com>
2019-05-13 10:43:27 -0400

    implement layout demo

--- bon-app/src/Planner.js
+++ bon-app/src/Planner.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const Planner = () => {
   return (
-    <div>
-      Planner
+    <div className="container">
+      <div class="row">
+        <form class="col s12">
+          <div class="row">
+            <div class="input-field col s12 m6">
+              <textarea id="mondayLunch" class="materialize-textarea" />
+              <label for="mondayLunch">Monday Lunch</label>
+            </div>
+            <div class="input-field col s12 m6">
+              <textarea id="mondayDiner" class="materialize-textarea" />
+              <label for="mondayDiner">Monday Diner</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12 m6">
+              <textarea id="tuesdayLunch" class="materialize-textarea" />
+              <label for="tuesdayLunch">Tuesday Lunch</label>
+            </div>
+            <div class="input-field col s12 m6">
+              <textarea id="tuesdayDiner" class="materialize-textarea" />
+              <label for="tuesdayDiner">Tuesday Diner</label>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Planner;
